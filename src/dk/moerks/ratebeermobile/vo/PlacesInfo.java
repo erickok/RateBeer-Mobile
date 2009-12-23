0f78fb9c022754a36e2426c5a18e60f7186e7875
jfm <jfm@jfm-laptop>
2009-12-23 10:38:26 +0100

    Added files for Places

---
+++ src/dk/moerks/ratebeermobile/vo/PlacesInfo.java
@@ -0,0 +1,6 @@
+package dk.moerks.ratebeermobile.vo;
+
+public class PlacesInfo {
+	//{"BrewerID":11,"BrewerName":"Anchor Brewing Company","BrewerCity":"San Francisco","Abbrev":"CA ","Country":"United States "}
+	
+}
