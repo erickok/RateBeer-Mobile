fd184185aeead02069fd108f95b5dfbd36de0c91
Eric Kok <erickok@gmail.com>
2010-04-08 12:01:42 +0200

    Ask to install the barcode scanner if not yet installed (and possible).

--- src/dk/moerks/ratebeermobile/Home.java
+++ src/dk/moerks/ratebeermobile/Home.java
@@ -18,10 +18,15 @@
  */
 package dk.moerks.ratebeermobile;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.SearchManager;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -48,6 +53,7 @@ import dk.moerks.ratebeermobile.task.RefreshFriendFeedTask.FriendFeedTaskResult;
 public class Home extends BetterRBListActivity {
 	private static final String LOGTAG = "Home";
 	private static final int BARCODE_ACTIVITY = 101;
+	private static final int INSTALL_BARCODE_SCANNER = 1;
 	
 	private boolean firstRun;
 	private boolean hasNewDrinkText;
@@ -83,21 +89,11 @@ public class Home extends BetterRBListActivity {
             }
         });
 
-    	// If barcode scanner is available, allow the scanner to be started
-        try {
-        	getPackageManager().getPackageInfo("com.google.zxing.client.android", PackageManager.GET_ACTIVITIES);
-
-        	barcodeMenuButton.setEnabled(true);
-        	barcodeMenuButton.setOnClickListener(new View.OnClickListener() {
-    			public void onClick(View v) {
-                    Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-                    startActivityForResult(intent, BARCODE_ACTIVITY);
-    			}
-            });
-        } catch(PackageManager.NameNotFoundException e){
-        	Log.d(LOGTAG, "BarcodeScanner is not installed");
-        	barcodeMenuButton.setEnabled(false);
-        }
+    	barcodeMenuButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+                tryStartBarcodeScanner();
+			}
+        });
         
         beermailButton.setOnClickListener(new View.OnClickListener() {
         	public void onClick(View v) {
@@ -198,6 +194,57 @@ public class Home extends BetterRBListActivity {
 		return false;
 	}
 
+	private void tryStartBarcodeScanner() {
+    	// If barcode scanner is available, allow the scanner to be started
+        try {
+        	getPackageManager().getPackageInfo("com.google.zxing.client.android", PackageManager.GET_ACTIVITIES);
+
+            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+            startActivityForResult(intent, BARCODE_ACTIVITY);
+            
+        } catch(PackageManager.NameNotFoundException e){
+        	
+        	Log.d(LOGTAG, "BarcodeScanner is not installed");
+        	// Ask if we should install the barcode scanner instead
+        	showDialog(INSTALL_BARCODE_SCANNER);
+        }
+	}
+	
+    @Override
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+		case INSTALL_BARCODE_SCANNER:
+			return buildInstallDialog(R.string.scan_scanner_not_found, Uri.parse("market://search?q=pname:com.google.zxing.client.android"));			
+		}
+		return null;
+	}
+
+	private Dialog buildInstallDialog(int messageResourceID, final Uri marketUri) {
+		AlertDialog.Builder fbuilder = new AlertDialog.Builder(this);
+		fbuilder.setMessage(messageResourceID);
+		fbuilder.setCancelable(true);
+		fbuilder.setPositiveButton(R.string.scan_install, new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+
+	        	try {
+					getPackageManager().getPackageInfo("com.google.zxing.client.android", PackageManager.GET_ACTIVITIES);
+					
+					Intent install = new Intent(Intent.ACTION_VIEW, marketUri);
+					startActivity(install);
+				} catch (NameNotFoundException e) {
+					Toast.makeText(getApplicationContext(), R.string.scan_nomarket, Toast.LENGTH_LONG).show();
+				}				
+				dialog.dismiss();
+			}
+		});
+		fbuilder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.cancel();
+			}
+		});
+		return fbuilder.create();
+	}
+    
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         if (requestCode == BARCODE_ACTIVITY) {
